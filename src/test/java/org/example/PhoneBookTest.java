a6a699554ddcd942ffee6c299dec3af30c4b6a0d
Elizaveta Zabolotnaia <zabolotnaya.ea@gmail.com>
2022-05-05 20:03:50 +0300

    some hamcrest tests added

--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -3,6 +3,14 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 class PhoneBookTest {
 
@@ -84,5 +92,38 @@ class PhoneBookTest {
 
         // expect:
         Assertions.assertNull(phoneBook.findContactByNumber(number));
+    }
+
+    @Test
+    void givenContacts_whenChecksSize_thenCorrect() {
+        phoneBook.addContact(new Contact("Philip", "467822"));
+        phoneBook.addContact(new Contact("Jane", "654567"));
+        phoneBook.addContact(new Contact("John", "256108"));
+        phoneBook.addContact(new Contact("Jack", "988264"));
+
+        assertThat(phoneBook.getContacts(), hasSize(4));
+    }
+
+    @Test
+    void contacts_whenEmpty_thenCorrect() {
+        assertThat(phoneBook.getContacts(), empty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("contactList")
+    void givenAContact_isPresent(Contact contact) {
+        Contact contact1 = new Contact("Philip", "327725");
+
+        phoneBook.addContact(contact);
+
+        assertThat(phoneBook.getContacts(), hasItem(contact));
+        assertThat(phoneBook.getContacts(), not(hasItem(contact1)));
+    }
+
+    private static List<Contact> contactList() {
+        return Arrays.asList(new Contact("Philip", "467822"),
+                new Contact("Jane", "654567"),
+                new Contact("John", "256108"),
+                new Contact("Jack", "988264"));
     }
 }
